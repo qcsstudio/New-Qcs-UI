dce8de75dda9a24399efb96921e041af470afa7c
pankajqcs <pankajqcss@gmail.com>
2025-12-03 18:04:07 +0530

    new section

--- src/app/startup-agency/page.tsx
+++ src/app/startup-agency/page.tsx
@@ -37,7 +37,7 @@ const index = () => {
             <VideoHomeTwo />
             <InnovativeHomeTwo />
             <MarqueeAreaHomeTwo />
-            <FeatureHomeTwo />
+            {/* <FeatureHomeTwo /> */}
             <ServiceHomeTwo />
             <PortfolioHomeOne />
             <AwardsHomeOne />
