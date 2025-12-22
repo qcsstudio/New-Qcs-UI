3102f96ae648d1c0f04b4fb507708c8a1e067f70
pankajqcs <pankajqcss@gmail.com>
2025-12-22 17:16:57 +0530

    add new policies and check suggestions

---
+++ src/app/compliance-regulatory/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PolicyPageTemplate from '../../components/PolicyComponent/PolicyPageTemplate'
+
+const page = () => {
+  return (
+    <>
+    <PolicyPageTemplate type="Compliance_regulatory" title="Compliance & Regulatory Policies" />;
+    </>
+  )
+}
+
+export default page
